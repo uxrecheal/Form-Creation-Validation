91b3c56ba366d6cbe59605cb8bfda379d8215895
Recheal Gademeh <rechealgademehetornam@gmail.com>
2024-10-13 17:22:41 +0000

    final commit

---
+++ fetch-data.js
@@ -0,0 +1,24 @@
+async function fetchUserData() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/users";
+  const dataContainer = document.getElementById("api-data");
+  try {
+    const response = await fetch(apiUrl);
+    const users = await response.json();
+    dataContainer.innerHTML = "";
+    const userList = document.createElement("ul");
+
+    users.forEach((element) => {
+      const li = document.createElement("li");
+      li.textContent = element.name;
+      userList.append(li);
+    });
+
+    dataContainer.append(userList);
+   
+  } catch (error) {
+    console.error(error);
+    dataContainer.innerHTML = '';
+    dataContainer.textContent = 'Failed to load user data.'
+  }
+}
+document.addEventListener('DOMContentLoaded',fetchUserData)
